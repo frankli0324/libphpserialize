ca9133a91c3b1fdc21fea70e04c0e7c2e6a59b37
Frank <frankli0324@hotmail.com>
2020-08-04 18:29:12 +0800

    adds new decorator for unserialization

--- phpserialize/decorators.py
+++ phpserialize/decorators.py
@@ -1,5 +1,12 @@
+from .unserialize import PHP_Class
+
+
 def namespace(ns):
     def _namespace_decorator(cls):
         cls.__namespace__ = ns
         return cls
     return _namespace_decorator
+
+
+def php_class(cls):
+    return type(cls.__name__, (cls, PHP_Class), {})
